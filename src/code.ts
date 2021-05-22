6361975a538989197ad10f12a4c121d2c726c7cc
LVignoni <leandrovignoni@gmail.com>
2021-05-22 19:41:44 -0300

    ex

--- src/code.ts
+++ src/code.ts
@@ -1,1 +1,33 @@
-import * as Rx from "rxjs"
+import { Observable } from 'rxjs';
+
+const hello = Observable.create((observer: any) => {
+    try {
+        observer.next('Hello');
+        observer.next('World');
+        setInterval(() => {
+            observer.next("Interval")
+        }, 2000)
+        // observer.complete();
+    } catch (error) {
+        observer.error(error)
+    }
+});
+
+let observer = hello.subscribe(
+    (arg: any) => addItem(arg),
+    (error: any) => addItem(error),
+    () => addItem("Completed")
+)
+
+setTimeout(() => {
+    observer.unsubscribe();
+}, 6001)
+
+function addItem(val: any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
+
+
